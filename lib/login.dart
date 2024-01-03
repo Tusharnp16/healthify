79434e1af684abf3c547880815360acfd3691183
Kamlesh <sardakamlesh3@gmail.com>
2024-01-03 11:43:52 +0530

    updated

--- lib/login.dart
+++ lib/login.dart
@@ -109,7 +109,7 @@ class loginscreen extends StatelessWidget{
                              height: 10,
                              thickness: 10,
                            ),
-                           const Text("Or continue with",
+                           const Text("Or login with",
                              style: TextStyle(
                                  fontSize: 18,
                                  color: Colors.black,
