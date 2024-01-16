d5017ed630d0e278cbf2646cac054801522ed7f3
Kamlesh <sardakamlesh3@gmail.com>
2024-01-16 18:53:25 +0530

    update of ABOUT us

---
+++ lib/Aboutus.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Aboutus extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Healthify is services application which will provide service such as :\n"
+                "1.Appointment BOOKING Service. \n"
+                "2.Hospital  &  Doctor Details. \n"
+                "3.Daily news update.",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.lightBlue,
+              ),),
+            Text(" FOUNDER OF HEALTHIFY : TUSHAR ,KAMLESH & HEMANT ",
+
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.lightBlue,
+              ),)
+          ],
+        ),
+      ),
+    );
+  }
+}
